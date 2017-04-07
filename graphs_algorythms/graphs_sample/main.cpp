552efc8cdfb408196325cd233f99646a3388a34d
alex960714 <alex_9607@mail.ru>
2017-04-07 18:40:30 +0300

    Added modofication for splited graphs

--- graphs_algorythms/graphs_sample/main.cpp
+++ graphs_algorythms/graphs_sample/main.cpp
@@ -12,6 +12,8 @@ void init_rand(int size, int perc);
 void init_bigraph(int size, int perc);
 void init_split(int size, int perc);
 void exper(int num, int size, int perc, int type);
+void exper_bigraph(int num, int size, int perc);
+void exper_split(int num, int size, int perc);
 
 int **matr;
 stack<int> st;
@@ -30,14 +32,9 @@ int main(int argc, char** argv)
 		cout << "Введите номер действия:" << endl << "1. Чтение из файла" << endl << "Заполнение случайными числами:"<<endl<<"2.Обыкновенный граф:" << endl;
 		cout << "3.Двудольный граф" << endl << "4.Расщепляемый граф" << endl;
 		cin >> op;
-		switch (op)
+
+		if (op >= 2 && op <= 4)
 		{
-		case 1:
-			size = init_file("graph_adj.txt");
-			break;
-		case 2:
-		case 3:
-		case 4:
 			cout << "Введите число вершин:" << endl;
 			cin >> size;
 			cout << "Введите процент плотности графа:" << endl;
@@ -48,43 +45,23 @@ int main(int argc, char** argv)
 			matr = new int*[size];
 			for (int i = 0; i < size; i++)
 				matr[i] = new int[size];
-
-			if (graph_num == 1)
-			{
-				init_rand(size, perc);
-			}
-			else
-			{
-				exper(graph_num, size, perc, op - 1);
-			}
-
+		}
+		switch (op)
+		{
+		case 1:
+		case 2:
+			exper(graph_num, size, perc, op);
 			break;
-		default:
-			cout << "Неправильный номер операции!" << endl;
+		case 3:
+			exper_bigraph(graph_num, size, perc);
+			break;
+		case 4:
+			exper_split(graph_num, size, perc);
 		}
+		
 	} while (op < 1 && op > 4);
-
-	if (graph_num == 1)
-	{
-		st_time = GetTickCount();
-		if (hamilton_cycle::search(matr, size, st))
-		{
-			en_time = GetTickCount();
-			printf("Hamilton cycle:\n");
-			while (!st.empty())
-			{
-				printf("%d ", st.top());
-				st.pop();
-			}
-			printf("\n");
-		}
-		else
-		{
-			en_time = GetTickCount();
-			printf("No hamilton cycle\n");
-		}
-		printf("Time: %f\n", (en_time - st_time) / 1000.0);
-	}
+	//exper(graph_num, size, perc, op - 1);
+	
 
 	for (int i = 0; i < size; i++)
 		delete[] matr[i];
@@ -219,18 +196,8 @@ void exper(int num, int size, int perc, int type)
 	os.open("results.txt");
 	for (int i = 0; i < num; i++)
 	{
-		switch (type)
-		{
-		case 1:
+		if (type == 2)
 			init_rand(size, perc);
-			break;
-		case 2:
-			init_bigraph(size, perc);
-			break;
-		case 3:
-			init_split(size,perc);
-			break;
-		}
 		if (size < 100)
 		{
 			for (int i = 0; i < size; i++)
@@ -266,6 +233,96 @@ void exper(int num, int size, int perc, int type)
 		
 		/*for (int i = 0; i < size; i++)
 			delete[] matr[i];
+		delete[] matr;*/
+
+		os << "---------------------------" << endl;
+	}
+
+	cout << endl << "Число графов с гамильтоновым циклом: " << cyc_num << endl;
+	cout << "Среднее время вычислений: " << avg_time << endl;
+
+	os << endl << "Число графов с гамильтоновым циклом: " << cyc_num << endl;
+	os << "Среднее время вычислений: " << avg_time << endl;
+	os.close();
+}
+
+//----------------------------------------------//
+
+void exper_bigraph(int num, int size, int perc)
+{
+	int cyc_num = 0;
+	double st_time, en_time, avg_time = 0;
+	ofstream os;
+	os.open("results.txt");
+	for (int i = 0; i < num; i++)
+	{
+		init_split(size, perc);
+
+		int curr_vert;
+		int* discover = new int[size];
+		discover[0] = 0;
+		for (int i = 0; i < size; i++)
+			discover[i] = -1;
+
+		curr_vert = 0;
+		int vert_num = 1;
+		while (vert_num < size)
+		{
+			for (int i = 0; i < size; i++)
+			{
+				if (matr[curr_vert][i])
+					discover[i] = (discover[curr_vert] + 1) % 2;
+			}
+		}
+	}
+}
+
+//---------------------------------------------//
+
+void exper_split(int num, int size, int perc)
+{
+	int cyc_num = 0;
+	double st_time, en_time, avg_time = 0;
+	ofstream os;
+	os.open("results.txt");
+	for (int i = 0; i < num; i++)
+	{
+		init_split(size, perc);
+		if (size < 100)
+		{
+			for (int i = 0; i < size; i++)
+			{
+				for (int j = 0; j < size; j++)
+				{
+					os << matr[i][j] << " ";
+				}
+				os << endl;
+			}
+		}
+		st_time = GetTickCount() / 1000.0;
+		if (perc >= 50 && hamilton_cycle::search(matr, size, st))
+		{
+			cyc_num++;
+			en_time = GetTickCount() / 1000.0;
+			os << "Hamilton cycle:" << endl;
+			while (!st.empty())
+			{
+				os << st.top() << " ";
+				st.pop();
+			}
+			os << endl;
+		}
+		else
+		{
+			en_time = GetTickCount() / 1000.0;
+			os << "No hamilton cycle" << endl;
+		}
+
+		os << "Time: " << (en_time - st_time) << endl;
+		avg_time = (avg_time*i + (en_time - st_time)) / (i + 1);
+
+		/*for (int i = 0; i < size; i++)
+		delete[] matr[i];
 		delete[] matr;*/
 
 		os << "---------------------------" << endl;
